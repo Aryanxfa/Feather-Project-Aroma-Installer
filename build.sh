e466c14a730c20e3d24674fed89644f3492873ca
Aryanxfa <aryan200152@gmail.com>
2023-01-11 20:45:59 +0530

    Change zip command

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 # To be used when ones images are ready
 # Call with the format ./build.sh FeatherProject_S22_T_v0.7.zip
 
-zip -v -r $1 META-INF system data boot.img dtb.img dtbo.img featherproject.keys LICENSE
+zip -v -r $1 META-INF system mods kernel img
