4a0264432786773532ff589b3c16c40e4af2b051
Aryanxfa <aryan200152@gmail.com>
2023-03-06 15:13:18 +0530

    build script:  Warn for Filename not provided

--- build.sh
+++ build.sh
@@ -4,4 +4,9 @@
 # To be used when ones images are ready
 # Call with the format ./build.sh FeatherProject_S22_T_v0.7.zip
 
+if [ -z "$1" ]
+then
+    echo "Filename not provided"
+    exit
+fi
 zip -v -r $1 META-INF mods kernel img aux featherproject.keys
