4ace449eaba365b04af4178e954e0b1756c21e25
Aryanxfa <aryan200152@gmail.com>
2023-03-10 11:36:33 +0530

    debloat: update debloater

--- debloat/nukeengine.sh
+++ debloat/nukeengine.sh
@@ -1,7 +1,7 @@
 #! /tmp/busybox sh
 
-cat /tmp/dblist | while read line 
+cat /tmp/dblist | while read line
 do
-   # do something with $line here
-   rm -rf "/system_root/system/$line"
+    # do something with $line here
+    rm "/system_root/system/$line"
 done
