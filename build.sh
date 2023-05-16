d8cc359ae1cbcf18baecd936b39500d83d142f45
Aryanxfa <aryan200152@gmail.com>
2023-05-16 15:41:43 +0530

    switch to continuation of 978d50a

--- build.sh
+++ build.sh
@@ -4,14 +4,12 @@
 # To be used when ones images are ready
 # Call with the format ./build.sh FeatherProject_S22_T_v0.7.zip
 
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
     echo "Filename not provided"
     exit
 fi
 
-if [ ! -f META-INF/scripts/xbin/sleep.kek.br ]
-then
+if [ ! -f META-INF/scripts/xbin/sleep.kek.br ]; then
     echo "Failed with error 1"
     exit
 fi
--- debloat/nukeengine.sh
+++ debloat/nukeengine.sh
@@ -1,7 +1,6 @@
 #! /tmp/busybox sh
 
-cat /tmp/dblist | while read line
-do
+cat /tmp/dblist | while read line; do
     # do something with $line here
     /tmp/busybox rm "/system_root/system/$line"
 done
