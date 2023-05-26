281577dda7310bdbb9f34dd5b63bb4b5dfa55d6d
Aryanxfa <aryan200152@gmail.com>
2023-05-26 14:24:55 +0530

    inject overlay during build

--- build.sh
+++ build.sh
@@ -14,4 +14,23 @@ if [ ! -f META-INF/scripts/xbin/sleep.kek.br ]; then
     exit
 fi
 
-zip -v -r $1 META-INF mods kernel img auxy csc debloat featherproject.keys
+#Pull overlays
+mkdir device/a10/overlay/
+cp ../FP_overlay/a10/framework-res__auto_generated_rro_vendor.apk device/a10/overlay/
+
+mkdir device/a20/overlay/
+cp ../FP_overlay/a20/framework-res__auto_generated_rro_vendor.apk device/a20/overlay/
+
+mkdir device/a20e/overlay/
+cp ../FP_overlay/a20e/framework-res__auto_generated_rro_vendor.apk device/a20e/overlay/
+
+mkdir device/a30/overlay/
+cp ../FP_overlay/a30/framework-res__auto_generated_rro_vendor.apk device/a30/overlay/
+
+mkdir device/a30s/overlay/
+cp ../FP_overlay/a30s/framework-res__auto_generated_rro_vendor.apk device/a30s/overlay/
+
+mkdir device/a40/overlay/
+cp ../FP_overlay/a40/framework-res__auto_generated_rro_vendor.apk device/a40/overlay/
+
+zip -v -r $1 META-INF mods kernel img device auxy csc debloat featherproject.keys
