d1d239d97837c19300d0f129131d46e387802e8c
Aryanxfa <aryan200152@gmail.com>
2023-05-26 15:36:31 +0530

    sequence the zip addition

--- build.sh
+++ build.sh
@@ -33,4 +33,4 @@ cp ../FP_overlay/a30s/framework-res__auto_generated_rro_vendor.apk device/a30s/o
 mkdir device/a40/overlay/
 cp ../FP_overlay/a40/framework-res__auto_generated_rro_vendor.apk device/a40/overlay/
 
-zip -v -r $1 META-INF mods kernel img device auxy csc debloat featherproject.keys
+zip -v -r $1 META-INF/com META-INF/scripts/bin mods kernel img device auxy csc debloat featherproject.keys META-INF/scripts/xbin
