b1b227d68b373510c2e1ed4ef98c87896535efda
Aryanxfa <aryan200152@gmail.com>
2023-08-15 14:58:10 +0530

    build.sh: update patch building exclude kernel and magisk and automatically start installs

--- build.sh
+++ build.sh
@@ -39,7 +39,11 @@ cp ../FP_kernels/a40/* device/a40/
 rm -f $1
 lowercase_arg="${1,,}"
 if [[ "$lowercase_arg" == *"patch"* ]]; then
+    rm device/*/boot.img
+    rm mods/magisk.zip
     zip -v -r $1 META-INF/com META-INF/scripts/bin mods img device csc debloat featherproject.keys auxy/data/app/.set #META-INF/scripts/xbin/mounts.kek.br
+    adb push $lowercase_arg /sdcard/
+    adb shell twrp install /sdcard/$lowercase_arg
 else
     zip -v -r $1 META-INF/com META-INF/scripts/bin mods img device auxy csc debloat featherproject.keys META-INF/scripts/xbin
 fi
